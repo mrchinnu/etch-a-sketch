2ec1dbe876838d8e4c880ea612f6d5bbad3f3729
mrchinnu <ganesh_mcr@srkrec.edu.in>
2024-09-28 06:32:59 -0400

    add button make grid dynamic add random color->box
    made grid size dynamic by adding a button to get user input
    added logic to make the colors on hover to appear randomly using js
    added border to container

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const box=document.createElement("div");
-box.classList.add("box");
-box.style.width(16);
-box.style.height(16);
-box.style.backgroundColor("blue");
+
 let container=document.querySelector(".container");
-let i=0;
-while(i<20){
-    container.appendChild(box);
+let getgrid=document.querySelector(".getgrid");
+container.addEventListener("mouseover",(event) => {
+    let i=0;
+    let color="rgba("
+    while(i<4){
+    a=Math.floor(Math.random()*255);
+    if(i<3){
+    color+=a+",";
+    }else{
+        color+=a+")"
+    }
     i++;
+    }
+    console.log(color);
+    event.target.style.background=color;
+  })
+getgrid.addEventListener("click", (event)=>{
+    container.replaceChildren();
+    generategrid(prompt("Enter no.of Squares in grid")-1+1);
+})
+function generategrid(numberofboxes=16){
+    if(numberofboxes>100){
+        alert("you cannot enter such high values, try numbers below 100");
+        return;
+    }
+    let i=0;
+    while(i<(numberofboxes**2)){
+        const box=document.createElement("div");
+        box.classList.add("box");
+        box.style.minWidth=600/numberofboxes+"px";
+        box.style.minHeight=600/numberofboxes+"px";
+        container.appendChild(box).cloneNode(true);
+        i++;
+    }      
 }
+generategrid();
